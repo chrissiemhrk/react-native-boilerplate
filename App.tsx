ba91b699a581c90002ee12f524ed2c52c67c8d40
chrissiemhrk <chrissiemhrk@gmail.com>
2022-03-04 19:37:02 +0200

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 import Navigation from "./src/components/navigation";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+const getFonts = () =>
+  Font.loadAsync({
+    Inter: require("./assets/fonts/Inter-Regular.ttf"),
+    Inter_Bold: require("./assets/fonts/Inter-Bold.ttf"),
+    Inter_Medium: require("./assets/fonts/Inter-Medium.ttf"),
+  });
 
 export default function App() {
-  return <Navigation />;
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
+
+  if (fontsLoaded) {
+    return <Navigation />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 }
